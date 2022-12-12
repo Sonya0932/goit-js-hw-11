e3b8da10f42a784b41ade0af0f8e1c5392487879
Sonya0932 <108592540+Sonya0932@users.noreply.github.com>
2022-12-12 22:30:26 +0200

    add

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export default {
-    searchForm: document.querySelector('#search-form'), 
-    gallery: document.querySelector('.gallery'),
-    btnLoad: document.querySelector('.btn-load-more'), 
-  };
